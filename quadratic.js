221ccc4b8ecef77851cad8485a71cb5f57965935
alantothe <rubenamalpartida@gmail.com>
2022-05-20 12:56:27 -0400

    updated quadratic js answer

--- quadratic.js
+++ quadratic.js
@@ -9,9 +9,9 @@ let c = 2
 
 let x = -b 
 
-x1 =  -b + Math.sqrt (b **2 + 4 * a + 4 * b) / 2 * a 
+x1 =  -b + Math.sqrt (b **2 - 4 * a + 4 * b) / 2 * a 
 
-x2 =  -b + Math.sqrt(b **2 - 4 * a + 4 * b) / 2 * a
+x2 =  -b - Math.sqrt(b **2 - 4 * a + 4 * b) / 2 * a
 
 console.log (" x1 is equal to " + x1 )
 console.log (" x2 is equal to " + x2 )
